aa80b2471020aa625cc2ac316d7b6a2d5eb48c40
Andres Roget <andres.roget@lightspeedpos.com>
2015-09-23 07:43:00 -0400

    aas

--- app/scripts/controllers/index.js
+++ app/scripts/controllers/index.js
@@ -8,38 +8,7 @@
  * Controller of the angularCountriesApp
  */
 angular.module('angularCountriesApp')
-  .controller('IndexCtrl', ['$scope', '$http', '$q', function ($scope, $http, $q) {
-
-      // var one = $q.defer();
-      //
-      // var all = $q.all([
-      //   $http.get("https://restcountries.eu/rest/v1/all", { cache: true}),
-      // ]);
-      //
-      // all.then(function(response){
-      //   var countries = response[0].data;
-      //   var data = [];
-      //
-      //   angular.forEach(countries, function(country, index) {
-      //     var image = function(country){
-      //       return $http.get('https://api.500px.com/v1/photos/search?term='+  country+'&tag=landscape&image_size=4&rpp=1&consumer_key=pxJyGsnSyQnwfT2xzDUie8L9lzym7FdVYLWGCA3H', { cache: true}).then(function(response) {
-      //
-      //         $scope.countries[index].image = response.data.photos[0].image_url;
-      //       });
-      //     }
-      //
-      //     data.push({
-      //       name : country.name,
-      //       capital : country.capital,
-      //       region : country.region,
-      //       image : image(country.name)
-      //     });
-      //   });
-      //
-      //   $scope.countries = data;
-      //   console.log($scope);
-      // })
-
+  .controller('IndexCtrl', ['$scope', '$http', function ($scope, $http) {
 
     function getCountries() {
       return $http.get("https://restcountries.eu/rest/v1/all", { cache: true});
@@ -77,7 +46,7 @@ angular.module('angularCountriesApp')
 
         });
       })
-      console.log($scope);
+
     }, function(reason) {
       console.log(reason);
     });
